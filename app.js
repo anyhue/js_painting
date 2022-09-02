92e135337d73c6f21a88bfa216f151b0912c4c38
anyhue <111332914+anyhue@users.noreply.github.com>
2022-09-02 12:38:14 +0900

    js_painting

---
+++ app.js
@@ -0,0 +1,50 @@
+// 캔버스 부르기
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+// 캔버스 크기 조정
+canvas.width = 800;
+canvas.height = 800;
+lineWidth = 2;
+
+const colors = [
+    "#f03e3e",
+    "#e64980",
+    "#be4bdb",
+    "#7950f2",
+    "#4c6ef5",
+    "#228be6",
+    "#15aabf",
+    "#12b886",
+    "#40c057",
+    "#82c91e",
+    "#fab005",
+    "#fd7e14"];
+
+let isPainting = false;
+
+function onMousemove (event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+    }
+
+    ctx.moveTo(event.offsetX, event.offsetY);
+    // const strokeColor = colors[Math.floor(Math.random() * colors.length)]
+    // ctx.strokeStyle = strokeColor;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
+
+function startPainting(event) {
+    isPainting = true;
+}
+
+function canclePainting(event) {
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMousemove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", canclePainting);
+canvas.addEventListener("mouseleave", canclePainting);
